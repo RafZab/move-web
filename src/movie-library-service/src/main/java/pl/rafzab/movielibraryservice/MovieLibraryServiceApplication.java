6c1b19d13dbcd234b412c06e76b38c17c83a2c6d
Rafal Zaborowski <raffik2000@gmail.com>
2025-07-31 19:20:46 +0200

    init spring boot app

---
+++ src/movie-library-service/src/main/java/pl/rafzab/movielibraryservice/MovieLibraryServiceApplication.java
@@ -0,0 +1,13 @@
+package pl.rafzab.movielibraryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieLibraryServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MovieLibraryServiceApplication.class, args);
+    }
+
+}
---
+++ src/movie-library-service/src/test/java/pl/rafzab/movielibraryservice/MovieLibraryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rafzab.movielibraryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieLibraryServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
